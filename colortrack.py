7d17be3afe5ae5669013d98d30d15c23b546c829
Snytbaggen <snytbaggen2@hotmail.com>
2015-04-10 17:44:23 +0000

    cleaned up colortrack.py, both code and comments

--- colortrack.py
+++ colortrack.py
@@ -8,8 +8,6 @@ import RPi as GPIO
 import cv2.cv as cv
 from pygame.locals import *
 
-#global frame, rgbj,LOW_TRESHOLD, HIGH_TRESHOLD, hsv_range, comm, serial, hsvi, previous_turn, size_error_previous, position_error_previous
-
 #Serial port setup
 comm.SetSerialPort('/dev/ttyUSB0',19200)
 time.sleep(2) #some delay necessary to prevent starting to talk with the arduino too fast
@@ -44,6 +42,7 @@ text_x_offset=5
 text_y_offset=15
 text_color=(128,255,255)
 
+#A pygame window is used to read the keyboard and show information
 pygame.init()
 screen = pygame.display.set_mode((160,120))
 pygame.display.set_caption ('Data window')
@@ -67,7 +66,6 @@ def ReadHsvValue(image,x,y):
 
 #Is called on mouse events.
 def MyMouseCallback(event, x, y, flags, param):
-    global frame
     if event==cv.CV_EVENT_LBUTTONDBLCLK:
         ReadHsvValue(frame,x,y)
 
@@ -90,24 +88,20 @@ def ColorProcess(img):
 #Updates the tresholds used in the ColorProcess function above, depending on the
 #hsv-range variable.
 def UpdateTreshold():
-    global LOW_TRESHOLD, HIGH_TRESHOLD, hsv_range
+    global LOW_TRESHOLD, HIGH_TRESHOLD
     LOW_TRESHOLD = [hsvi[0]-hsv_range, hsvi[1]-hsv_range,hsvi[2]-hsv_range]
     HIGH_TRESHOLD = [hsvi[0]+hsv_range, hsvi[1]+hsv_range,hsvi[2]+hsv_range]
 
-#Calculates if the robot should move forward or not depending on the size of the ball.
-#A simple PD controller is used for this, with the values Kp and Kd chosen by experimentation.
-#Just a P controller is probably sufficient with good lighting (and a fast connection),
-#but increasing the Kd value might be a good idea if there is a low framerate and/or
-#a long input delay from the camera. Because of weak motors no PWM is used, the motor is
-#set to either full speed or full stop. 
+# Calculates if the robot should move forward or backwards depending on ball size.
+# A simple PD controller is used with values chosen from experimentation. Only full stop
+# and full speed is used because of weak motors. The protocol supports PWM.
 def CalculateMove(radius):
     global size_error_previous
-    Kp = 1
-    Kd = 10
-
+    Kp = 1  #Proportional constant
+    Kd = 10 #Derivative constant
     
     target_size = 250 #Target radius for the ball, chosen by experiment
-    cutoff = 40 #Cutoff to counter "jitter" in the size, chosen by experiment 
+    cutoff = 40 #Cutoff to counter "jitter" in the size caused by camera, chosen by experiment 
 
     size_error_current = target_size - radius
     size_error_derivative = size_error_current-size_error_previous
@@ -124,19 +118,16 @@ def CalculateMove(radius):
     size_error_previous = size_error_current
     return move_value
     
-#Calculates if the robot should turn or not depending on the X-position of the ball.
-#A simple PD controller is used for this, with the vaules Kp and Kd chosen by experimentation.
-#Just a P controller is probably sufficient with good lighting (and a fast connection),
-#but increasing the Kd value might be a good idea if there is a low framerate and/or
-#a long input delay from the camera. Because of weak motors no PWM is used, the motor is
-#set to either full speed or full stop. 
+# Calculates if the robot should move left or right depending on ball position.
+# A simple PD controller is used with values chosen from experimentation. Only full stop
+# and full speed is used because of weak motors. The protocol supports PWM.. 
 def CalculateTurn(current_position):
-    global position_error_previous, width
-    Kp = -4
-    Kd = -2
+    global position_error_previous
+    Kp = -4 #Proportional constant
+    Kd = -2 #Derivative constant
 
-    target_position = width/2 #width/2
-    cutoff = 50
+    target_position = width/2
+    cutoff = 50 # Cutoff to counter "jitter" in the position caused by camera, chosen by experiment
 
     position_error_current = target_position - current_position
     position_error_derivative = position_error_current - position_error_previous
@@ -159,15 +150,14 @@ def CalculateMovement(posX, posY, radius):
     if move < 0:
         turn = -turn
 
-    #If the ball is far down the area is smaller so the robot
-    #thinks it's far away, a cutoff is added when the ball position
-    #is too far down
+    # If the ball is too close the camera might cut it off making it appear to be far away
+    # because of a smaller area. To prevent this the robot doesn't move if the Y position is above
+    # a certain treshold (Y=0 at top of the image)
     if posY > 85:
         move = 0
     comm.SendMoveCommand(move, turn)
 
 def exit_program():
-    global comm
     comm.SendMoveCommand(0,0)
     comm.HeadlightsOff()
     comm.Disconnect()
@@ -201,9 +191,12 @@ headlights_on = True
 #Main loop. Code for fetching and processing the image has been copied and slightly modified from code
 #written by Oscar Liang that was uploaded online
 while True:
+    #Read keyboard buttons
     events = pygame.event.get()
     for event in events:
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN: #Key press is detected
+            #Read directional buttons, increase sensitivity/range or
+            #set move/turn directions depending on manual control is off or on
             if event.key == pygame.K_UP:
                 if manual_control:
                     arrow_keys[0] = True
@@ -227,46 +220,37 @@ while True:
                 else:
                     blob_sensitivity -= 250
 
-            if event.key == pygame.K_t:
+            #Read other relevant keys
+            if event.key == pygame.K_t: #Toggle tracking
                 hsv_tracking = not hsv_tracking
-            if event.key == pygame.K_v:
+            if event.key == pygame.K_v: #Toggle video feed
                 video_feed = not video_feed
-            if event.key == pygame.K_m:
+            if event.key == pygame.K_m: #Toggle manual control
                 manual_control = not manual_control
-            if event.key == pygame.K_h:
+            if event.key == pygame.K_h: #Toggle headlights
                 if (headlights_on):
                     comm.HeadlightsOff()
                 else:
                     comm.HeadlightsOn()
                 headlights_on = not headlights_on
-            if event.key == pygame.K_q:
+            if event.key == pygame.K_q: #Quit
                 exit_program()
 
-            if event.key == pygame.K_KP7:
-                Kp += 1
-            if event.key == pygame.K_KP4:
-                Kp -= 1
-            if event.key == pygame.K_KP8:
-                Kd += 1
-            if event.key == pygame.K_KP5:
-                Kd -= 1
-
-        if event.type == pygame.KEYUP:
+        if event.type == pygame.KEYUP: #Key release is detected
+            #Read directional arrows, disable turn/move directions
+            #for pressed buttons.
             if event.key == pygame.K_UP:
-                if manual_control:
-                    arrow_keys[0] = False
+                arrow_keys[0] = False
             if event.key == pygame.K_DOWN:
-                if manual_control:
-                    arrow_keys[1] = False
+                arrow_keys[1] = False
             if event.key == pygame.K_LEFT:
-                if manual_control:
-                    arrow_keys[2] = False
+                arrow_keys[2] = False
             if event.key == pygame.K_RIGHT:
-                if manual_control:
-                    arrow_keys[3] = False
+                arrow_keys[3] = False
 
     background.fill((0,0,0))
-    #Fetch frame and blur it. Copied code starts here.
+    #Fetch frame and blur it. 
+    #Copied code starts here:
     frame = cv.QueryFrame(capture)
     cv.Smooth(frame, frame, cv.CV_BLUR, 3)
 
@@ -279,43 +263,39 @@ while True:
     moment10 = cv.GetSpatialMoment(moments, 1, 0)
     moment01 = cv.GetSpatialMoment(moments, 0,1)
 
-    # Find a big enough blob
-    #The area condition has been modified to account for user-chosen
-    #sensitivity. The area value has also been adjusted for the different
-    #camera resolution used in this project.
+    # Find a big enough blob. The area condition has been modified to account for
+    # user-chosen sensitivity and the different camera resolution used in this code
     if(area > 15000+blob_sensitivity):
 
         # Calculating the center postition of the blob
         posX = int(moment10 / area)
         posY = int(moment01 / area) 
         #Copied code ends here
-        
 
-        #Update HSV value from middle of blob.
-        #Kind of inaccurate, an average value would be a lot better
+        # Update HSV value from middle of blob.
+        # Inaccurate, an average value would be a lot better
         if hsv_tracking:
             ReadHsvValue(frame,posX,posY)
         
-        #Calculate a value from the area proportional to the object's radius, assuming
-        #it is a circle. Then draw a circle for visual aid around the object, with the
-        #divisor 15 choosen by experiment to be "good enough".
+        # Calculate a value from the area proportional to the object's radius, assuming
+        # it is a circle. Then draw a circle for visual aid around the object, with the
+        # divisor 15 choosen by experiment to be "good enough".
         radius = int(math.sqrt(area/math.pi))
         cv.Circle(frame, (posX, posY), radius/15, (0,0,255), 2,8,0)
 
-        #TODO: Put regulation and robot movement here
         if not manual_control:
-            CalculateMovement(posX, posY, radius)
+            CalculateMovement(posX, posY, radius) #Calculates move and turn values
 
-        #If a blob has been found then also write its position in text on the processed image.
+        #If a blob has been found, write its position in text on the processed image.
         background.blit(pygamefont.render("blob x, y, r: "+str(posX)+", "+str(posY)+", "+str(radius), 1, (255,255,255)),(text_x_offset,text_y_offset-10))
     else:
-        #If a blob haven't been found then indicate that by writing "N/A" instead of position data.
+        #If a blob haven't been found, indicate that by writing "N/A" instead of position data.
         background.blit(pygamefont.render("blob x, y: N/A", 1, (255,255,255)),(text_x_offset,text_y_offset-10))
         comm.SendMoveCommand(0,0)
 
         size_error_previous = size_error_integral = 0 #Also reset the regulation variables to avoid junk data later
 
-    #Write the rest of the information
+    #Write other information on screen
     background.blit(pygamefont.render("hsv-range: "+str(hsv_range), 1, (255,255,255)),(text_x_offset,text_y_offset*2-10))
     background.blit(pygamefont.render("sensitivity: "+str(-1*blob_sensitivity/250), 1, (255,255,255)),(text_x_offset,text_y_offset*3-10))
 
@@ -337,10 +317,10 @@ while True:
         background.blit(pygamefont.render("Headlights: on", 1, (255,255,255)), (text_x_offset, text_y_offset*7-10))
     else:
         background.blit(pygamefont.render("Headlights: off", 1, (255,255,255)), (text_x_offset, text_y_offset*7-10))
-
     screen.blit(background, (0,0))
     pygame.display.flip()
-
+    
+    #Set turn values if manual control is enabled
     if manual_control:
         keys = pygame.key.get_pressed()
         turn = move = 0
@@ -360,11 +340,6 @@ while True:
         cv.ShowImage("Original", frame)
     else:
         cv.ShowImage("Original", blank_image)
-
-    #arrow_keys = [False, False, False, False]
     
-    #Wait for key press, and if a key has been pressed then act accordingly.
-    #This function call is very important because it also handles a lot of stuff
-    #regarding the windows.
-    pygame.event.pump()
-    key = cv.WaitKey(50)
+    pygame.event.pump()  #Load key events
+    key = cv.WaitKey(10) #Handles important things, among other things images will not show if this isn't called.
